76e337ce2b04dcb4e63a4e17374849d82c9f7c46
farisikhhaq <farisikhlasulhaq@yahoo.co.id>
2021-04-22 16:43:01 +0700

    Jobsheet 11 (Prak 2 : Modified handler)

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,8 +2,12 @@
 
 namespace App\Exceptions;
 
+use Facade\FlareClient\Http\Response;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Http\Response as HttpResponse;
 use Throwable;
+
 
 class Handler extends ExceptionHandler
 {
@@ -37,5 +41,16 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+    public function render($request, Throwable $exception)
+    {
+        if($exception instanceof ModelNotFoundException && $request->wantsJson()){
+            return response()->json(
+                ['message' => 'Not Found!'],
+                HttpResponse::HTTP_NOT_FOUND
+            );
+        }
+
+        return parent::render($request, $exception);
     }
 }
